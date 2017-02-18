8cb0a652a81d2f2b6e4f1e33f5be21e59a18c380
tleavitt <tucker.leavitt@gmail.com>
2017-02-18 15:24:59 -0800

    add hanblebars templating

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-var express = require('express')
-var app = express()
+
+'use strict';
+
+var express = require('express'),
+    exphbs  = require('express-handlebars'); // "express-handlebars"
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    res.render('home');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    console.log('express-handlebars example server listening on: 3000');
+});
