e3d798ceeab07f157737f12a8fe75d4703f0c2d4
tleavitt <tucker.leavitt@gmail.com>
2017-02-18 16:38:25 -0800

    video grid

--- app.js
+++ app.js
@@ -1,18 +1,82 @@
 
 'use strict';
 
-var express = require('express'),
-    exphbs  = require('express-handlebars'); // "express-handlebars"
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
 
-var app = express();
+const express = require('express'),
+    exphbs  = require('express-handlebars'), // "express-handlebars"
+    Handlebars = require('handlebars')
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+
+	const videos = [0, 1, 2, 3, 4, 5, 6].map(makeVideoCell)
+	const body = makeVideoGrid(videos)
+    res.send(makePage(body))
 });
 
+
 app.listen(3000, function () {
-    console.log('express-handlebars example server listening on: 3000');
+    console.log('server listening on: 3000');
 });
+
+
+
+function makeVideoGrid(videos) {
+
+    const rows = []
+    for (var i = 0; i < videos.length/2 - 1; i++) {
+        rows.push({'html':'<div class="row">{0}{1}</div>'.format(videos[2*i], videos[2*i+1])})
+    }
+    if (rows.length % 2 == 1) {
+        rows.push({'html':'<div class="row">{0}</div>'.format(videos[videos.length - 1])})
+    }
+
+	const source = '<div id="wrap">{{#items}}{{{html}}}{{/items}}</div>'    
+    const template = Handlebars.compile(source);
+    const context = {'items': rows}
+    const result = template(context)
+    console.log(result)
+    return result
+}
+
+function makeVideoCell(url) {
+	return '<div class="noclick video"> \
+		<iframe width="400" \
+			height="300" \
+			src="https://www.youtube.com/embed/kfchvCyHmsc?loop=1&rel=0&version=3&autoplay=1&controls=0&showinfo=0&playlist=kfchvCyHmsc" \
+			frameborder="0"> \
+		</iframe>\
+	</div>'
+}
+
+const makePage = (body) => '\
+	<!DOCTYPE html> \
+	<html> \
+	<head> \
+	  <meta charset="utf-8"> \
+	  <title>Example App</title> \
+	  <link rel="stylesheet" type="text/css" href="css/style.css" /> \
+	</head> \
+	  <body> \
+	\
+	    {0} \
+	\
+	  </body>\
+	</html>'.format(body)
